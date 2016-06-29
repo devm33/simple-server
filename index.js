293dc95f3e5884ff6f66209a3996f6f33b9b8ef6
Devraj Mehta <devm33@gmail.com>
2016-06-29 01:05:38 -0400

    make command line options

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 /* origin server for a file cdn */
 
-// TODO make these configurable command line args
-var PORT = 8080;
-var FILE_ROOT = __dirname + '/root';
-var URL_ROOT = '/root';
+var argv = require('yargs')
+  .default('port', 8080)
+  .default('root', __dirname + '/root')
+  .default('url', 'root')
+  .argv;
 
 var express = require('express');
 var fs = require('fs');
@@ -18,18 +19,18 @@ app.get('/', function(req, res) {
 
 app.put('/*', function(req, res) {
   var reqPathDirname = req.path.substr(0, req.path.lastIndexOf('/'));
-  mkdirp(FILE_ROOT + reqPathDirname, function (err) {
+  mkdirp(argv.root + reqPathDirname, function (err) {
     if (err) {
       console.error(err);
     } else {
-      req.pipe(fs.createWriteStream(FILE_ROOT + req.path));
+      req.pipe(fs.createWriteStream(argv.root + req.path));
       res.send(req.path);
     }
   });
 });
 
-app.use(URL_ROOT, express.static(FILE_ROOT));
+app.use('/' + argv.url, express.static(argv.root));
 
-app.listen(PORT, function () {
-  console.log('simple server started at http://0.0.0.0:' + PORT);
+app.listen(argv.port, function () {
+  console.log('server started at http://localhost:' + argv.port);
 });
